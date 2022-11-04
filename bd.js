bb0e06f63d3598f1bf9efc2ecd4c4d5b1ec70903
maycanin <maysoninacio@gmail.com>
2022-11-04 11:51:53 -0300

    sistemas de pesquisas no banco de dados

--- bd.js
+++ bd.js
@@ -1,13 +1,28 @@
 var bd = openDatabase("meuBD", "1.0", "Meu Banco de Dados", 4080);
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE nomes (nome TEXT)");
+    criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER)");
 });
 
-function cadastraNome() {
+function salvarDados() {
     const nome = document.getElementById("nome").value;
+    const idade = parseInt(document.getElementById("idade").value);
 
     bd.transaction(function (inserir) {
-        inserir.executeSql("INSERT INTO nomes (nome) VALUES (?)", [nome]);
+        inserir.executeSql(
+            "INSERT INTO formulario (nome, idade) VALUES (?, ?)",
+            [nome, idade]
+        );
+    });
+}
+
+function pesquisarPorNome() {
+    const nome = document.getElementById("pesquisa-nome").value;
+
+    bd.transaction(function (ler) {
+        ler.executeSql(`SELECT * FROM formulario WHERE nome="${nome}"`, [], function (ler, resultados){
+            const tamanho = resultados.rows.length;
+            console.log(tamanho);
+        });
     });
 }
--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function converteParaFahrenheit() {
     const temperaturaCelsius = parseFloat(document.getElementById("celsius").value);
     
     const temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
-    document.getElementById('resultado-fahrenheit').innerHTML = 'O resultado é ' + temperaturaFahrenheit.toFixed(2) + 'º Fahrenheit'
+    document.getElementById('resultado-fahrenheit').value = temperaturaFahrenheit.toFixed(2) + 'º Fahrenheit';
 }
