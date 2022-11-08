f9d080498d30bca11d22888c078bd5d059958e7f
maycanin <maysoninacio@gmail.com>
2022-11-08 11:49:10 -0300

    Edição no BD

--- bd.js
+++ bd.js
@@ -5,24 +5,56 @@ bd.transaction(function (criar) {
 });
 
 function salvarDados() {
-    const nome = document.getElementById("nome").value;
+    const nome = document.getElementById("nome").value.toUpperCase();
     const idade = parseInt(document.getElementById("idade").value);
 
+    if (nome === "" || isNaN(idade)) {
+        alert("Informe corretamente os dados");
+        return false;
+    }
     bd.transaction(function (inserir) {
         inserir.executeSql(
             "INSERT INTO formulario (nome, idade) VALUES (?, ?)",
             [nome, idade]
         );
     });
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
 }
 
 function pesquisarPorNome() {
-    const nome = document.getElementById("pesquisa-nome").value;
+    const nome = document.getElementById("pesquisa-nome").value.toUpperCase();
 
     bd.transaction(function (ler) {
-        ler.executeSql(`SELECT * FROM formulario WHERE nome="${nome}"`, [], function (ler, resultados){
-            const tamanho = resultados.rows.length;
-            console.log(tamanho);
-        });
+        ler.executeSql(
+            `SELECT * FROM formulario WHERE nome LIKE "%${nome}%"`,
+            [],
+            function (ler, resultados) {
+                const tamanho = resultados.rows.length;
+
+                const msg = tamanho + " linha(s) contada(s)"
+                console.log(msg);
+
+                const nome = resultados.rows.item(tamanho - 1).nome;
+                const idade = resultados.rows.item(tamanho - 1).idade;
+                document.getElementById('pesquisa-nome').value = `${nome}`;
+                document.getElementById('resultado-pesquisa').value = idade + " anos";
+            }
+        );
     });
+}
+
+function exibeBD() {
+    document.getElementById("lista-bd").innerHTML = ""
+    bd.transaction(function (exibe) {
+        exibe.executeSql("SELECT * FROM formulario", [], function (exibe, resultados){
+           const tamanho = resultados.rows.length;
+           
+           let item;
+           for (let i = 0; i < tamanho; i++){
+            item = resultados.rows.item(i);
+            document.getElementById("lista-bd").innerHTML += `<p>Nome: ${item.nome}, ${item.idade} anos</p>`
+           }
+        })
+    })
 }
