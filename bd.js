966fb412dcaac85a523dd4c96b97d270f21219b1
maycanin <maysoninacio@gmail.com>
2022-11-11 11:48:32 -0300

    novos inputs e colunas no bd

--- bd.js
+++ bd.js
@@ -1,29 +1,41 @@
 var bd = openDatabase("meuBD", "1.0", "Meu Banco de Dados", 4080);
 
 bd.transaction(function (criar) {
-    criar.executeSql("CREATE TABLE formulario (nome TEXT, idade INTEGER, email TEXT, telefone INTEGER)");
+    criar.executeSql(
+        "CREATE TABLE formulario (nome TEXT, idade INTEGER, email TEXT, telefone TEXT, cpf TEXT)"
+    );
 });
 
 function salvarDados() {
     const nome = document.getElementById("nome").value.toUpperCase();
     const idade = parseInt(document.getElementById("idade").value);
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
+    const email = document.getElementById("email").value.toLowerCase();
+    const telefone = document
+        .getElementById("telefone")
+        .value.replace(/\D/g, "");
+    const cpf = document.getElementById("cpf").value.replace(/[^\d]+/g, "");
 
-    if (nome === "" || isNaN(idade) || email === "" || isNaN(telefone)) {
+    if (
+        nome === "" ||
+        isNaN(idade) ||
+        email === "" ||
+        isNaN(telefone) ||
+        isNaN(cpf)
+    ) {
         alert("Informe corretamente os dados");
         return false;
     }
     bd.transaction(function (inserir) {
         inserir.executeSql(
-            "INSERT INTO formulario (nome, idade, email, telefone) VALUES (?, ?, ?, ?)",
-            [nome, idade, email, telefone]
+            "INSERT INTO formulario (nome, idade, email, telefone, cpf) VALUES (?, ?, ?, ?, ?)",
+            [nome, idade, email, telefone, cpf]
         );
     });
     document.getElementById("nome").value = "";
     document.getElementById("idade").value = "";
     document.getElementById("email").value = "";
     document.getElementById("telefone").value = "";
+    document.getElementById("cpf").value = "";
 }
 
 function pesquisarPorNome() {
@@ -31,7 +43,7 @@ function pesquisarPorNome() {
 
     bd.transaction(function (ler) {
         ler.executeSql(
-            `SELECT * FROM formulario WHERE nome LIKE "%${nome}%"`,
+            `SELECT * FROM formulario WHERE nome LIKE "${nome}%"`,
             [],
             function (ler, resultados) {
                 const tamanho = resultados.rows.length;
@@ -39,11 +51,18 @@ function pesquisarPorNome() {
                 const msg = tamanho + " linha(s) contada(s)";
                 console.log(msg);
 
-                const nome = resultados.rows.item(tamanho - 1).nome;
-                const idade = resultados.rows.item(tamanho - 1).idade;
-                document.getElementById("pesquisa-nome").value = `${nome}`;
-                document.getElementById("resultado-pesquisa").value =
-                    idade + " anos";
+                let item;
+                for (let i = 0; i < tamanho; i++) {
+                    item = resultados.rows.item(i);
+                    document.getElementById(
+                        "lista-bd"
+                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}', ${item.idade}, '${item.email}', ${item.telefone}, ${item.cpf})" style="cursor: pointer;">${item.nome} - ${item.idade} anos - ${item.email} - ${item.telefone} - ${item.cpf}</p>`;
+                }
+                // const nome = resultados.rows.item(tamanho - 1).nome;
+                // const idade = resultados.rows.item(tamanho - 1).idade;
+                // document.getElementById("pesquisa-nome").value = `${nome}`;
+                // document.getElementById("resultado-pesquisa").value =
+                //     idade + " anos";
             }
         );
     });
@@ -63,7 +82,7 @@ function exibeBD() {
                     item = resultados.rows.item(i);
                     document.getElementById(
                         "lista-bd"
-                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}', ${item.idade})" style="cursor: pointer;">Nome: ${item.nome}, ${item.idade} anos</p>`;
+                    ).innerHTML += `<p onclick="mostrarCartaoAltera('${item.nome}', ${item.idade}, '${item.email}', ${item.telefone}, ${item.cpf})" style="cursor: pointer;">${item.nome} - ${item.idade} anos - ${item.email} - ${item.telefone} - ${item.cpf}</p>`;
                 }
             }
         );
@@ -75,31 +94,56 @@ function alteraInfo() {
     const idadeAlteracao = parseInt(
         document.getElementById("idade-alteracao").value
     );
-    if (nomeAlteracao === "" || isNaN(idadeAlteracao)) {
+    const emailAlteracao = document.getElementById("email-alteracao").value;
+    const telefoneAlteracao = document
+        .getElementById("telefone-alteracao")
+        .value.replace(/\D/g, "");
+    const cpfAlteracao = document
+        .getElementById("cpf-alteracao")
+        .value.replace(/[^\d]+/g, "");
+
+    if (
+        nomeAlteracao === "" ||
+        isNaN(idadeAlteracao) ||
+        emailAlteracao === "" ||
+        isNaN(telefoneAlteracao) ||
+        isNaN(cpfAlteracao)
+    ) {
         alert("Informe corretamente os dados");
         return false;
     }
+
     bd.transaction(function (modifica) {
         modifica.executeSql(
-            `UPDATE formulario SET nome="${nomeAlteracao}", idade=${idadeAlteracao} WHERE nome="${nomeParaEditar}" AND idade=${idadeParaEditar}`
+            `UPDATE formulario SET nome="${nomeAlteracao}", idade=${idadeAlteracao}, email="${emailAlteracao}", telefone=${telefoneAlteracao}, cpf=${cpfAlteracao} WHERE nome="${nomeParaEditar}" AND idade=${idadeParaEditar} AND email="${emailParaEditar}" AND telefone=${telefoneParaEditar} AND cpf=${cpfParaEditar}`
         );
     });
-    exibeBD()
+
+    exibeBD();
     document.getElementById("nome-alteracao").value = "";
     document.getElementById("idade-alteracao").value = "";
-    const alteraBD = document.getElementById('altera-bd');
+    document.getElementById("email-alteracao").value = "";
+    document.getElementById("telefone-alteracao").value = "";
+    document.getElementById("cpf-alteracao").value = "";
+
+    const alteraBD = document.getElementById("altera-bd");
     alteraBD.style.display = "none";
 }
 
 function excluirInfo() {
     bd.transaction(function (exclui) {
         exclui.executeSql(
-            `DELETE FROM formulario WHERE nome="${nomeParaEditar}" AND idade=${idadeParaEditar}`
+            `DELETE FROM formulario WHERE nome="${nomeParaEditar}" AND idade=${idadeParaEditar} AND email="${emailParaEditar}" AND telefone=${telefoneParaEditar} AND cpf=${cpfParaEditar}`
         );
     });
-    exibeBD()
+
+    exibeBD();
     document.getElementById("nome-alteracao").value = "";
     document.getElementById("idade-alteracao").value = "";
-    const alteraBD = document.getElementById('altera-bd');
+    document.getElementById("email-alteracao").value = "";
+    document.getElementById("telefone-alteracao").value = "";
+    document.getElementById("cpf-alteracao").value = "";
+
+    const alteraBD = document.getElementById("altera-bd");
     alteraBD.style.display = "none";
 }
--- script.js
+++ script.js
@@ -1,21 +1,118 @@
 let nomeParaEditar;
 let idadeParaEditar;
+let emailParaEditar;
+let telefoneParaEditar;
+let cpfParaEditar;
 
-function converteParaFahrenheit() {
-    const temperaturaCelsius = parseFloat(document.getElementById("celsius").value);
-    
-    const temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
-    document.getElementById('resultado-fahrenheit').value = temperaturaFahrenheit.toFixed(2) + 'º Fahrenheit';
-}
-
-
-function mostrarCartaoAltera(nome, idade) {
+function mostrarCartaoAltera(nome, idade, email, telefone, cpf) {
     document.getElementById("nome-alteracao").value = nome;
     nomeParaEditar = nome;
-    
+
     document.getElementById("idade-alteracao").value = idade;
     idadeParaEditar = parseInt(idade);
-    
-    const alteraBD = document.getElementById('altera-bd');
+
+    document.getElementById("email-alteracao").value = email;
+    emailParaEditar = email;
+
+    document.getElementById("telefone-alteracao").value = telefone;
+    telefoneParaEditar = telefone;
+
+    document.getElementById("cpf-alteracao").value = cpf;
+    cpfParaEditar = cpf;
+
+    const alteraBD = document.getElementById("altera-bd");
     alteraBD.style.display = "flex";
 }
+
+const handlePhone = (event) => {
+    let input = event.target;
+    input.value = phoneMask(input.value);
+};
+
+const phoneMask = (value) => {
+    if (!value) return "";
+    value = value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d)/, "($1) $2");
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+    return value;
+};
+
+const handleCpf = (event) => {
+    let input = event.target;
+    input.value = cpfMask(input.value);
+};
+
+const cpfMask = (cpf) => {
+    if (!cpf) return "";
+
+    cpf = cpf.replace(/[^\d]+/g, "");
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    return cpf;
+    // value = value.replace(/\D/g, "");
+    // value = value.replace(/(\d{2})(\d)/, "($1) $2");
+    // value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+    // return value;
+};
+
+// function verificarCPF(cpf) {
+//     // Remove os pontos/traço da expressão regular, caso exista
+//     cpf = cpf.replace(/[^\d]+/g, "");
+//     if (cpf == "") return false;
+
+//     // Elimina CPFs invalidos conhecidos
+//     if (
+//         cpf.length != 11 ||
+//         cpf == "00000000000" ||
+//         cpf == "11111111111" ||
+//         cpf == "22222222222" ||
+//         cpf == "33333333333" ||
+//         cpf == "44444444444" ||
+//         cpf == "55555555555" ||
+//         cpf == "66666666666" ||
+//         cpf == "77777777777" ||
+//         cpf == "88888888888" ||
+//         cpf == "99999999999"
+//     )
+//         return false;
+
+//     // Valida 1o digito
+//     add = 0;
+
+//     for (i = 0; i < 9; i++) {
+//         add += parseInt(cpf.charAt(i)) * (10 - i);
+//     }
+//     rev = 11 - (add % 11);
+//     if (rev == 10 || rev == 11) {
+//         rev = 0;
+//     }
+
+//     if (rev != parseInt(cpf.charAt(9))) {
+//         return false;
+//     }
+
+//     // Valida 2o digito
+//     add = 0;
+//     for (i = 0; i < 10; i++) {
+//         add += parseInt(cpf.charAt(i)) * (11 - i);
+//     }
+//     rev = 11 - (add % 11);
+//     if (rev == 10 || rev == 11) {
+//         rev = 0;
+//     }
+//     if (rev != parseInt(cpf.charAt(10))) {
+//         return false;
+//     }
+//     return true;
+// }
+
+// function converteParaFahrenheit() {
+//     const temperaturaCelsius = parseFloat(
+//         document.getElementById("celsius").value
+//     );
+
+//     const temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
+//     document.getElementById("resultado-fahrenheit").value =
+//         temperaturaFahrenheit.toFixed(2) + "º Fahrenheit";
+// }
